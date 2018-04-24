ff22c68e810b02f55f89993aa8ea8cd02e609311
christian.passold <christian.passold@hbsis.com.br>
2018-04-24 07:38:11 -0300

    Busca em profundidade

--- src/main/java/src/busca/BuscaProfundidade.java
+++ src/main/java/src/busca/BuscaProfundidade.java
@@ -35,7 +35,6 @@ public class BuscaProfundidade implements Busca {
         final Iterator<VerticeBuscaProfundidade> vertices = grafo.getVertices();
         while (vertices.hasNext()) {
             final VerticeBuscaProfundidade vertice = vertices.next();
-            System.out.println("Vertice Inicial: " + vertice);
             if (vertice.getCor().equals(Cor.Branco)) {
                 DFS(vertice);
             }
@@ -43,27 +42,21 @@ public class BuscaProfundidade implements Busca {
     }
 
     private void DFS(VerticeBuscaProfundidade vertice) {
-        System.out.println("v: " + vertice);
         this.tempo++;
         vertice.setTempoDescoberta(tempo);
         vertice.setCor(Cor.Cinza);
-        try {
-            Iterator<VerticeBuscaProfundidade> it = grafo.getVerticesAdjacentes(vertice);
-            while (it.hasNext()) {
-                VerticeBuscaProfundidade v = it.next();
 
-                if (v.getCor().equals(Cor.Branco)) {
-                    System.out.println("-- adj: " + v);
-                    v.setPai(vertice);
-                    DFS(v);
-                }
+        Iterator<VerticeBuscaProfundidade> it = grafo.getVerticesAdjacentes(vertice);
+        while (it.hasNext()) {
+            VerticeBuscaProfundidade v = it.next();
+
+            if (v.getCor().equals(Cor.Branco)) {
+                v.setPai(vertice);
+                DFS(v);
             }
-        } catch (Exception e) {
-            System.out.println("Vertice <" + vertice + "> não tem adjacente.");
         }
 
         vertice.setCor(Cor.Preto);
-        System.out.println("vertice preto: " + vertice);
         this.tempo++;
         vertice.setTempoFinalizacao(tempo);
     }
