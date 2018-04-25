2ea0c04e787c88ded7504982b12fdc643b3cad0b
Christian Passold <passold.christian@hotmail.com>
2018-04-25 20:50:42 -0300

    Refatoração

--- src/test/java/src/TesteInformacaoArquivo.java
+++ src/test/java/src/TesteInformacaoArquivo.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 import src.core.ERepresentacao;
 import src.core.vertices.VerticeBuscaLargura;
+import src.core.vertices.VerticeBuscaProfundidade;
 import src.factory.GrafoFactory;
 import src.grafos.AbstractGrafo;
 import src.input.GrafoFromFile;
@@ -22,7 +23,7 @@ public class TesteInformacaoArquivo {
     }
 
     @Test
-    public void InformationGrafoToFileTest() throws IOException {
+    public void InformationGrafoToFileLABL() throws IOException {
 
         final AbstractGrafo<VerticeBuscaLargura> grafo = GrafoFactory.constroiGrafo(ERepresentacao.LISTA_ADJACENCIA, VerticeBuscaLargura.class);
 
@@ -33,6 +34,51 @@ public class TesteInformacaoArquivo {
             adjacentes.forEachRemaining(destino -> grafo.adicionarAresta(new VerticeBuscaLargura(origem), new VerticeBuscaLargura(destino)));
         });
 
-        InformationGrafoToFile.toFile(grafo, "grafo.out");
+        InformationGrafoToFile.toFile(grafo, "InformationGrafoToFileLABL.out");
+    }
+
+    @Test
+    public void InformationGrafoToFileMABL() throws IOException {
+
+        final AbstractGrafo<VerticeBuscaLargura> grafo = GrafoFactory.constroiGrafo(ERepresentacao.MATRIZ_ADJACENCIA, VerticeBuscaLargura.class);
+
+        //setup grafo
+        grafo.setQuantidadeVertices(grafoFromFile.getQuantidadeVertices());
+        grafoFromFile.getVertices().forEachRemaining(origem -> {
+            Iterator<String> adjacentes = grafoFromFile.getAdjacentes(origem);
+            adjacentes.forEachRemaining(destino -> grafo.adicionarAresta(new VerticeBuscaLargura(origem), new VerticeBuscaLargura(destino)));
+        });
+
+        InformationGrafoToFile.toFile(grafo, "InformationGrafoToFileMABL.out");
+    }
+
+    @Test
+    public void InformationGrafoToFileLABP() throws IOException {
+
+        final AbstractGrafo<VerticeBuscaProfundidade> grafo = GrafoFactory.constroiGrafo(ERepresentacao.LISTA_ADJACENCIA, VerticeBuscaProfundidade.class);
+
+        //setup grafo
+        grafo.setQuantidadeVertices(grafoFromFile.getQuantidadeVertices());
+        grafoFromFile.getVertices().forEachRemaining(origem -> {
+            Iterator<String> adjacentes = grafoFromFile.getAdjacentes(origem);
+            adjacentes.forEachRemaining(destino -> grafo.adicionarAresta(new VerticeBuscaProfundidade(origem), new VerticeBuscaProfundidade(destino)));
+        });
+
+        InformationGrafoToFile.toFile(grafo, "InformationGrafoToFileLABP.out");
+    }
+
+    @Test
+    public void InformationGrafoToFileMABP() throws IOException {
+
+        final AbstractGrafo<VerticeBuscaProfundidade> grafo = GrafoFactory.constroiGrafo(ERepresentacao.MATRIZ_ADJACENCIA, VerticeBuscaProfundidade.class);
+
+        //setup grafo
+        grafo.setQuantidadeVertices(grafoFromFile.getQuantidadeVertices());
+        grafoFromFile.getVertices().forEachRemaining(origem -> {
+            Iterator<String> adjacentes = grafoFromFile.getAdjacentes(origem);
+            adjacentes.forEachRemaining(destino -> grafo.adicionarAresta(new VerticeBuscaProfundidade(origem), new VerticeBuscaProfundidade(destino)));
+        });
+
+        InformationGrafoToFile.toFile(grafo, "InformationGrafoToFileMABP.out");
     }
 }
